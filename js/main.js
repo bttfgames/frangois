f1847e2705067f600bacb37b402c03ecdbbe42f1
Ellison Leão <ellisonleao@gmail.com>
2012-01-30 11:11:32 -0200

    cleaning project / changed loading screen to default one

--- js/main.js
+++ js/main.js
@@ -1,51 +1,68 @@
-/*!
- *
- *   melonJS
- *   http://www.melonjs.org
- *
- *   Step by step game creation tutorial
- *
- **/
- 
-// game ressources
+/*
+*
+*  Frangoi's Journey
+*  This code is under the MIT License
+*  Copyright (C) 2012 Fuschia Team
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+**/
+
+
+//Game resources
 var g_ressources = [
-	{name: "font",         type: "image",   src: "images/font.png"},
-	{name: "levelcomplete",         type: "image",   src: "images/levelcomplete.png"},
-	{name: "levelcompletebg",         type: "image",   src: "images/levelcompletebg.png"},
-	{name: "creditos",         type: "image",   src: "images/creditos.png"},
-	{name: "menu",         type: "image",   src: "images/menu.png"},
-    {name: "titlescreen",  type: "image",   src: "images/titlescreen.png"},
-	{name: "menu_arrow",   type: "image",  src: "images/bolinha_menu.png"},
-	{name: "p_layer2",     type: "image",  src: "images/layer2.png"},
-	{name: "p_layer3",     type: "image",  src: "images/layer3.png"},
-	{name: "pl01",     type: "image",  src: "images/background/frente.png"},
-	{name: "pl02",     type: "image",  src: "images/background/chao.png"},
-	{name: "pl03",     type: "image",  src: "images/background/torre.png"},
-	{name: "pl04",     type: "image",  src: "images/background/montanha.png"},
-	{name: "pl05",     type: "image",  src: "images/background/ceu.png"},
-	{name: "area01_bkg0",     type: "image",  src: "images/area01_bkg0.png"},
-	{name: "spinning_coin_gold",     type: "image",  src: "images/coin.png"},
-	{name: "monstro1",     type: "image",  src: "images/monstro2.png"},
-	
+	{name: "font",				type: "image",src: "images/font.png"},
+	{name: "levelcomplete",		type: "image",src: "images/levelcomplete.png"},
+	{name: "levelcompletebg",	type: "image",src: "images/levelcompletebg.png"},
+	{name: "creditos",			type: "image",src: "images/creditos.png"},
+	{name: "menu",				type: "image",src: "images/menu.png"},
+    {name: "titlescreen",		type: "image",src: "images/titlescreen.png"},
+	{name: "menu_arrow",		type: "image",src: "images/bolinha_menu.png"},
+	//Parallel BGs
+	{name: "pl01",				type: "image",src: "images/background/frente.png"},
+	{name: "pl02",				type: "image",src: "images/background/chao.png"},
+	{name: "pl03",				type: "image",src: "images/background/torre.png"},
+	{name: "pl04",				type: "image",src: "images/background/montanha.png"},
+	{name: "pl05",     			type: "image",src: "images/background/ceu.png"},
+	{name: "spinning_coin_gold",type: "image",src: "images/coin.png"},
+
 	// our level tileset
-    {name: "tileset-platformer",  type:"image",	src: "images/tileset-platformer.png"},
+    {name: "tileset-platformer",type:"image",src: "images/tileset-platformer.png"},
 	//foreground
-	{name: "frente",  type:"image",	src: "images/background/frente.png"},
+	{name: "frente",  			type:"image",src: "images/background/frente.png"},
     // our levels
-    {name: "fase1",              type: "tmx",	src: "maps/fase1.tmx"},
-	{name: "velho3",     type: "image",  src: "images/velho3.png"},
-	
-	//sons
+    {name: "fase1",             type: "tmx",src: "maps/fase1.tmx"},
+	//chars
+	{name: "velho3",     		type: "image",  src: "images/velho3.png"},
+	{name: "monstro1",			type: "image",  src: "images/monstro2.png"},
+
+	//sounds
 	{name: "limbo",     type: "audio",  src: "sounds/", channel:1},
-	{name: "physic",     type: "audio",  src: "sounds/", channel:1},
-	{name: "transition",     type: "audio",  src: "sounds/", channel:1},
-	{name: "wee",     type: "audio",  src: "sounds/", channel:2},
+	{name: "physic",    type: "audio",  src: "sounds/", channel:1},
+	{name: "transition",type: "audio",  src: "sounds/", channel:1},
+	{name: "wee",     	type: "audio",  src: "sounds/", channel:2},
 	{name: "cling",     type: "audio",  src: "sounds/", channel:2},
 	{name: "stomp",     type: "audio",  src: "sounds/", channel:2},
-	{name: "jump",     type: "audio",  src: "sounds/", channel:2},
-	
+	{name: "jump",     	type: "audio",  src: "sounds/", channel:2},
+
 	//hud
-	{name: "barra",  type: "image",   src: "images/barra.png"},
+	{name: "barra",  	 type: "image",   src: "images/barra.png"},
 	{name: "barra-a-0",  type: "image",   src: "images/barra-a-0.png"},
 	{name: "barra-a-1",  type: "image",   src: "images/barra-a-1.png"},
 	{name: "barra-a-2",  type: "image",   src: "images/barra-a-2.png"},
@@ -64,20 +81,16 @@ var g_ressources = [
 	{name: "barra-b-6",  type: "image",   src: "images/barra-b-6.png"},
 	{name: "barra-b-7",  type: "image",   src: "images/barra-b-7.png"},
 	{name: "barra-b-8",  type: "image",   src: "images/barra-b-8.png"},
-	
+
 ];
 
 
 var jsApp	=
 {
-	/* ---
-
-		Initialize the jsApp
-
-		---			*/
 	onload: function()
 	{
 
+		//DEBUG mode
 		//me.debug.renderHitBox = true;
 
 		// init the video
@@ -97,17 +110,11 @@ var jsApp	=
 		me.loader.preload(g_ressources);
 
 		// load everything & display a loading screen
-		me.state.set(me.state.LOADING, new LoadingScreen());
-
+		//me.state.set(me.state.LOADING, new LoadingScreen());
 		me.state.change(me.state.LOADING);
 	},
 
-
-	/* ---
-
-		callback when everything is loaded
-
-		---										*/
+	//Callback when its loaded
 	loaded: function ()
 	{
 		//SCREENS
@@ -115,16 +122,13 @@ var jsApp	=
 		me.state.set(me.state.CREDITS, new CreditsScreen());
 		me.state.set(me.state.PLAY, new PlayScreen());
 		me.state.set(me.state.READY, new LevelCompleteScreen());
-		
-		// set the "Play/Ingame" Screen Object
-		//me.state.set(me.state.PLAY, new PlayScreen());
 
 		me.entityPool.add("mainPlayer", PlayerEntity);
 		me.entityPool.add("coinEntity", CoinEntity);
 		me.entityPool.add("foregroundEntity", ForegroundEntity);
 		me.entityPool.add("enemyEntity", EnemyEntity);
 		me.entityPool.add("tmxlevelcomplete", TMXLevelEntity);
-		
+
 		me.state.transition("fade", "#FFFFFF", 15);
 
 		// enable the keyboard
@@ -135,7 +139,11 @@ var jsApp	=
         me.input.bindKey(me.input.KEY.ENTER,"enter", true);
         me.input.bindKey(me.input.KEY.X,"x", true);
 		me.input.bindKey(me.input.KEY.C,"jump", true);
+
+		//DEBUG key
 		me.input.bindKey(me.input.KEY.K,"k", true);
+
+		//Pause screen
 		me.state.onPause = function () {
             context = me.video.getScreenFrameBuffer();
             context.fillStyle = "rgba(0, 0, 0, 0.8)";
@@ -146,20 +154,28 @@ var jsApp	=
             font.draw(context, "P A U S E", (me.video.getWidth() / 2) - (measure.width / 2), (me.video.getHeight() / 2) - (measure.height / 2))
         };
 
+		//Global Game Vars
 		me.gamestat.add("spirit_value",0);
 		me.gamestat.add("total_coins",0);
 		me.totalcoins = 10;
+
 		// start the game
 		me.state.change(me.state.MENU);
 	}
 
-}; // jsApp
+};
+
+/*
+*
+* Game Screens
+*
+*/
+
+//LOADING
 
 var LoadingScreen = me.ScreenObject.extend({
    // constructor
    init: function(){
-      // pass true to the parent constructor
-      // as we draw our progress bar in the draw function
       this.parent(true);
       this.font = new me.BitmapFont("font", 32);
       this.logo = new me.Font('font', 32);
@@ -178,14 +194,11 @@ var LoadingScreen = me.ScreenObject.extend({
    // make sure the screen is only refreshed on load progress
    update: function() {
       if (this.invalidate===true) {
-
          this.invalidate = false;
          return true;
       }
-
       return false;
    },
-
 
    onDestroyEvent : function () {
       // "nullify" all fonts
@@ -193,7 +206,7 @@ var LoadingScreen = me.ScreenObject.extend({
    },
 
    draw : function(context) {
-      me.video.clearSurface(context, "white");
+      me.video.clearSurface(context, "back");
       this.font.draw(context, "LOADING..", 320,240);
       var width = Math.floor(this.loadPercent * context.canvas.width);
       context.strokeStyle = "silver";
@@ -203,7 +216,9 @@ var LoadingScreen = me.ScreenObject.extend({
    },
 });
 
-//Menu
+
+
+//MENU
 var TitleScreen = me.ScreenObject.extend({
     init: function() {
         this.parent(true);
@@ -224,12 +239,12 @@ var TitleScreen = me.ScreenObject.extend({
             this.menuItems[0] = new me.Vector2d(200, 350);
             this.menuItems[1] = new me.Vector2d(200, 415);
         }
-		
+
 		this.tween = new me.Tween(this.arrow.pos).to({
 			x: 200
 		},400).onComplete(this.tweenbw.bind(this)).start();
-		
-		
+
+
         this.arrow.pos.y = this.menuItems[this.selectedItem].y;
 		pb = new me.ParallaxBackgroundEntity(1);
         pb.addLayer("menu", 1, 1);
@@ -237,20 +252,20 @@ var TitleScreen = me.ScreenObject.extend({
 
         me.game.add(this.arrow, 100);
 	},
-	
+
 	tweenbw: function(){
 		this.tween.to({
 			x:190
 		},400).onComplete(this.tweenff.bind(this)).start();
 	},
-	
+
 	tweenff: function(){
 		this.tween.to({
 			x:200
 		},400).onComplete(this.tweenbw.bind(this)).start();
 	},
-	
-	
+
+
     update: function() {
 		if (me.input.isKeyPressed("up")) {
             if (this.selectedItem > 0) {
@@ -269,7 +284,6 @@ var TitleScreen = me.ScreenObject.extend({
             return true;
         }
         if (me.input.isKeyPressed("enter")) {
-            //me.audio.play("menu", false);
             if (this.selectedItem == 0) {
                 me.state.change(me.state.PLAY)
             }
@@ -283,18 +297,13 @@ var TitleScreen = me.ScreenObject.extend({
     },
 
     draw: function(context) {
-        //context.drawImage(this.title, (context.canvas.width - this.title.width) / 2, 80);
         this.font.draw(context, " ", 270, 350);
         this.font.draw(context, " ", 245, 415);
     },
 
-    onDestroyEvent: function() {
-         
-    }
-
 });
 
-
+//LEVEL CheckPoint
 var TMXLevelEntity = me.LevelEntity.extend({
     init: function (a, c, b) {
         this.parent(a, c, b);
@@ -304,11 +313,11 @@ var TMXLevelEntity = me.LevelEntity.extend({
         this.collidable = false;
 		//if (me.totalcoins == 0)
 			me.state.change(me.state.READY);
-		
+
     }
 });
 
-
+//LEVEL COMPLETE
 LevelCompleteScreen = me.ScreenObject.extend({
     init: function () {
         this.parent(true);
@@ -318,6 +327,7 @@ LevelCompleteScreen = me.ScreenObject.extend({
         this.tween = null;
         this.background = null;
     },
+
     onResetEvent: function (a) {
         if (this.font == null) {
             this.title = me.loader.getImage("levelcomplete");
@@ -329,38 +339,40 @@ LevelCompleteScreen = me.ScreenObject.extend({
             postitle: 150
         }, 750).onComplete(this.tweencb.bind(this));
         this.tween.easing(me.Tween.Easing.Bounce.EaseOut);
-        me.input.bindKey(me.input.KEY.ENTER, "enter");
-        me.input.bindKey(me.input.KEY.X, "enter");
         this.tween.start();
+        me.input.bindKey(me.input.KEY.ENTER, "enter");
+        me.input.bindKey(me.input.KEY.ESC, "enter");
+
 		me.game.disableHUD();
         pb.addLayer("levelcompletebg", 1, 1);
 		me.game.add(pb);
     },
+
     tweencb: function () {
         this.tweenfinished = true
     },
-	
+
     update: function () {
         if (me.input.isKeyPressed("enter")) {
             me.state.change(me.state.PLAY);
         }
         return true
     },
+
     draw: function (a) {
-        //a.drawImage(this.background.canvas, 0, 0);
         a.drawImage(this.title, (a.canvas.width - this.title.width) / 2, this.postitle);
         this.font.set("left");
     },
+
     onDestroyEvent: function () {
-        //me.input.unbindKey(me.input.KEY.ENTER);
-        //me.input.unbindKey(me.input.KEY.X);
-        this.tween.stop()
+        me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindKey(me.input.KEY.ESC);
+        this.tween.stop();
     }
 });
 
 
-
-//Créditos
+//CREDITS
 CreditsScreen = me.ScreenObject.extend({
     init: function () {
         this.parent(true);
@@ -374,29 +386,27 @@ CreditsScreen = me.ScreenObject.extend({
             this.font.set("center");
         }
         me.input.bindKey(me.input.KEY.ESC, "enter", true);
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+
 		pb = new me.ParallaxBackgroundEntity(1);
         pb.addLayer("creditos", 1, 1);
-        //pb.addLayer("p_layer3", 2, 2);
         me.game.add(pb);
     },
     update: function () {
         if (me.input.isKeyPressed("enter")) {
-			//me.audio.play("menu", false);
             me.state.change(me.state.MENU);
         }
-        me.game.viewport.pos.y += 1;
         return true
     },
-    draw: function (a) {
-        //a.drawImage(this.title, (a.canvas.width - this.title.width) / 2, 80);
-		//this.font.draw(a, "SUNGA COLLECTIVE", 30, 150);
-    },
-    onDestroyEvent: function () {
-        //me.audio.playTrack("Menu_Song")
+
+	onDestroyEvent: function () {
+		me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindKey(me.input.KEY.ESC);
     }
+
 });
 
-//tela principal
+//PLAY SCREEN
 var PlayScreen = me.ScreenObject.extend({
 
 	onResetEvent: function(){
@@ -406,10 +416,10 @@ var PlayScreen = me.ScreenObject.extend({
 		me.game.currentLevel.getLayerByName("b_s").visible = false;
 		me.game.currentLevel.getLayerByName("f_s").visible = false;
 		me.audio.playTrack("physic");
-		console.log(me.game.getEntityByName("coinEntity"));
 		me.gamestat.setValue('total_coins',me.game.getEntityByName("coinEntity").length);
+
 	},
-	
+
 	onDestroyEvent: function(){
 		me.audio.stopTrack();
     }
@@ -426,13 +436,12 @@ var HUDSpiritObject = me.HUD_Item.extend({
     },
 
     draw: function (c, b, d) {
-        var a = this.pos.x + b;
-		//console.log(this.value);
+        var a = this.pos.x;
 		this.spiritIcon = me.loader.getImage("barra-a-"+this.value);
-        c.drawImage(this.spiritIcon, a, this.pos.y + d);
+        c.drawImage(this.spiritIcon, a, this.pos.y);
     }
 });
-	
+
 
 window.onReady(function()
 {
